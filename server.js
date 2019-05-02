62a2334829ed944bab83a02aafbfa6914722c917
Ansgar Reimann <ansgar.reimann@protonmail.com>
2019-05-02 15:41:32 +0200

    Add Jerrys solution and update gitignore

--- server.js
+++ server.js
@@ -27,3 +27,27 @@ fs.readFile(__dirname + "/counter.txt", "UTF8", (err, data) => {
     );
   }
 });
+
+/* from jerry
+
+const fs = require('fs')
+​
+const filePath = __dirname + '/counter.txt'
+​
+fs.readFile(filePath, 'utf8', (err, text) => {
+ if (err) {
+  writeFile(0)
+ } else {
+  const startIndex = text.lastIndexOf(' ')
+  const num = Number(text.slice(startIndex + 1))
+  writeFile(num + 1)
+ }
+})
+​
+function writeFile(count) {
+ fs.writeFile(filePath, `Last count was ${count}`, err => {
+  console.log('File created', filePath, count)
+ })
+}
+
+*/
