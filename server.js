11d108f6164c995156a8b0e924f6a013de8d72bd
Ansgar Reimann <ansgar.reimann@protonmail.com>
2019-05-02 21:25:56 +0200

    Add simple frontend

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require("express");
+const uid = require("uid");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.static("./dist"));
+
 let i = 0;
+let users = [
+  { name: "Joe", role: "mechanic", id: "140" },
+  { name: "Dr. Who", role: "time traveler", id: "150" },
+  { name: "Aragorn", role: "king", id: "160" },
+  { name: "Bart Simpson", role: "eat my shorts", id: "170" }
+];
 
 app.listen(3000, err => {
   err ? console.log(err) : console.log("Server ready");
 });
 app.get("/", (req, res) => {
   res.send(`Hello world ${i++}`);
+});
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.json(users.find(user => user.id === id));
+});
+
+app.post("/users", (req, res) => {
+  users = [...users, req.body];
+  users.forEach(item => (item.id = uid()));
+
+  res.json(users.find(user => req.body.name === user.name));
 });
 /*const fs = require("fs");
 
